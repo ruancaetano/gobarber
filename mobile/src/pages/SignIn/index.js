61be314af3e20de447858cd7f83bb4087f2ef1fc
Ruan Caetano <ruansouza_caetano@hotmail.com>
2020-02-09 18:48:35 -0300

    Adds mobile button and input  components

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>sdasd</Text>
+      <Text>SignIn</Text>
+      <Input
+        icon="call"
+        placeholder="Digite seu nome"
+        style={{ marginTop: 40 }}
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
